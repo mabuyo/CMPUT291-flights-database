2d1394ee88168e67071971ef8c700e7aba45e1f1
Anju Eappen <aeappen@ualberta.ca>
2015-10-25 01:45:25 -0600

    Lots of changes
    1. Changed to Bradley's data...good_connection has output now
    2. added to flight search query, it returns exactly the number of
    flights as the original, but as soon as you ask for the number of seats,
    it freaks out...
    3. a lot of sql output files have been generated, most of these
    were for testing if the data returned is consistent with the original
    views

---
+++ src/search.sql
@@ -0,0 +1,16 @@
+drop view good_connections;
+create view good_connections (src,dst,dep_date,flightno1,flightno2, layover,price, dep_time, arr_time, seats) as
+SELECT DISTINCT ff.src, ff.dst, ff.dep_date, ff.no1, ff.no2, ff.layover, ff.price, ff.dep_time, ff.arr_time , CASE WHEN a3.seats <= a4.seats then a3.seats else a4.seats end 
+FROM
+(select a1.src, a2.dst, a1.dep_date, a1.flightno AS no1, a2.flightno AS no2, a2.dep_time-a1.arr_time as layover,
+  min(a1.price+a2.price) AS price, a1.dep_time, a2.arr_time, a2.dep_date as date2 
+from available_flights a1, available_flights a2
+where a1.dst=a2.src and a1.arr_time +1.5/24 <=a2.dep_time and a1.arr_time +5/24 >=a2.dep_time
+group by a1.src, a2.dst, a1.dep_date, a1.flightno, a2.flightno, a2.dep_time, a1.arr_time, a1.dep_time, a2.arr_time, a2.dep_date
+) ff,  
+available_flights a3, available_flights a4
+WHERE a3.dep_date = ff.dep_date and a3.flightno = ff.no1 AND a4.flightno = ff.no2 AND a4.dep_date = ff.date2 AND a3.dep_time = ff.dep_time AND a4.arr_time = ff.arr_time;
+
+spool new_gc2.txt
+select * from good_connections;
+spool off;
