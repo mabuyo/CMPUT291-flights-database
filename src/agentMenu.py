5c427a9aab2f35abe995351ea60484731e5b292c
mabuyo <mabuyo@ualberta.ca>
2015-10-27 15:41:08 -0600

    Cleaned up agent menu.

--- src/agentMenu.py
+++ src/agentMenu.py
@@ -2,12 +2,14 @@ import userMenu
 import main
 
 class AgentMenu(userMenu.UserMenu):
+    """
+    AgentMenu inherits from UserMenu with added functionality of recording an actual departure and actual arrival time.
+    """
     def __init__(self, email):
         super().__init__(email)
 
     def showMenu(self):
         # user menu plus more
-        # record a flight departure
         while True:
             userInput = input("Search for flights (S) or List existing bookings (B), Logout (L)?  ")
             if (userInput == "S"):
@@ -20,12 +22,12 @@ class AgentMenu(userMenu.UserMenu):
                 self.showExistingBookings()
                 self.promptForBooking()
             elif (userInput == "RD"):
-                print("Recording flight departure.\n")
+                print("Recording flight departure. ")
                 (fno, date) = self.findFlight()
                 update = self.promptUpdate()
                 self.recordDep(fno, date, update)
             elif (userInput == "RA"):
-                print("Recording flight arrival. \n")
+                print("Recording flight arrival. ")
                 (fno, date) = self.findFlight()
                 update = self.promptUpdate()
                 self.recordArr(fno, date, update)
@@ -36,7 +38,6 @@ class AgentMenu(userMenu.UserMenu):
                 print("Pick a valid option. \n")
 
     def findFlight(self):
-        #TODO, update the searching for flights maybe???
         while True:
             flight = input("Enter the flight number and departure date (DD-MON-YYYY), separated using a space. (R) to return.  ")
             if flight == 'R': self.showMenu()
@@ -52,19 +53,17 @@ class AgentMenu(userMenu.UserMenu):
 
     def promptUpdate(self):
         while True:
-            date = input("Record the actual departure time (DD-MON-YYY, HH:MI)")
+            date = input("Record the actual time (DD-MON-YYY, HH:MI): ")
             if date == 'R': self.showMenu()
             return date
 
     def recordDep(self, fno, date, update):
-        # UPDATE sch_flights SET act_dep_time = TO_DATE(update, 'DD-MON-YYYY, HH:MI') WHERE flightno = input AND dep_date = TO_DATE(date,'DD-MON-YYYY')
         record = "UPDATE sch_flights SET act_dep_time = TO_DATE('" + update + "', 'DD-MON-YYYY, hh24:mi') WHERE (flightno = '" + fno +  "' AND dep_date = TO_DATE('" + date + "', 'DD-MON-YYYY'))"
         db = main.getDatabase()
         db.execute(record)
         db.execute("commit")
         print("Successfully recorded actual departure time.")
         self.showMenu()
-
 
     def recordArr(self, fno, date, update):
         record = "UPDATE sch_flights SET act_arr_time = TO_DATE('" + update + "', 'DD-MON-YYYY, hh24:mi') WHERE (flightno = '" + fno +  "' AND dep_date = TO_DATE('" + date + "', 'DD-MON-YYYY'))"
